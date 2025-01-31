707b7536afc5db3cc063d389f211d4225119ed75
charansai1310 <kondapanenicharansai@gmail.com>
2025-01-31 01:16:02 -0600

    Describe the changes you made

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,12 @@ import TimeLineComponent from "./TimeLineComponent";
 
 const Home = () => {
   return (
-    <div className="trump">
-      <h2>The Second Presidential Term of Donald J. Trump (2025 - 2029)</h2>
-      <div className="home-partition">
+    <div className="trump fade-in">
+      <div className="background-overlay"></div>
+      <h2 className="title-animate">
+        The Second Presidential Term of Donald J. Trump (2025 - 2029)
+      </h2>
+      <div className="home-partition slide-in">
         <div className="time-line">
           <TimeLineComponent />
         </div>
